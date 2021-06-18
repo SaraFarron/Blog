d283174396a1d1ee5e614a835030671495613e0d
Nikita Syulin <nikita_sulin@idpowers.com>
2021-06-18 16:38:17 +0700

    migrated to postgre

--- Blog/settings.py
+++ Blog/settings.py
@@ -81,11 +81,15 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-# TODO Switch to postgres
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
