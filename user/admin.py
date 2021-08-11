ddbdbbc7b489acfd5d2506459f7d6ea63f6ca362
Nikita Syulin <nikita_sulin@idpowers.com>
2021-08-11 14:38:03 +0700

    test git

--- user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Guest
-
-
-@admin.register(Guest)
-class GuestAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date_created', 'email')
-    list_filter = ('name', 'date_created', 'email')
