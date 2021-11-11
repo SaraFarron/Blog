817354e56d70324ca4c643986335543d405c851e
Nikita Syulin <nikita_sulin@idpowers.com>
2021-11-11 13:01:08 +0700

    reset branch 4

--- blog/urls.py
+++ blog/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('user/', include('user.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
