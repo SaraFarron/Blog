f3543832347ea2010237198042c70addfcf751f9
Nikita Syulin <nikita_sulin@idpowers.com>
2021-11-17 13:26:23 +0700

    change null parameters

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -5,10 +5,10 @@ from user.models import Guest
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, default='Untitled')
-    creation_date = models.DateTimeField(auto_now_add=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     description = models.TextField(max_length=200)
-    user = models.ForeignKey(Guest, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(Guest, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
