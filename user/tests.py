e7bc7c994b329e5d34ebf8138455e6f529d5c347
Nikita Syulin <nikita_sulin@idpowers.com>
2022-01-23 16:22:53 +0700

    tests

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .views import *
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_retrieve(self):
+        pass
+
+    def test_list(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_partial_update(self):
+        pass
+
+    def test_destroy(self):
+        pass
