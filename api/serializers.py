b1ae83576df6ff37956d6d33902ff894c625dfc8
Nikita Syulin <nikita_sulin@idpowers.com>
2022-01-23 15:18:24 +0700

    tests 1 and serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,22 @@
-from rest_framework.fields import CharField
-from rest_framework.serializers import (ModelSerializer, StringRelatedField, SlugRelatedField, )
+from rest_framework.fields import CharField, IntegerField
+from rest_framework.serializers import ModelSerializer, StringRelatedField, SlugRelatedField
+
 from BlogApp.models import Post, Comment, Guest
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = ('name', 'email', 'phone', 'skype', 'is_banned', 'is_muted', 'last_ban_date', 'is_moderator')
 
 
 class PostSerializer(ModelSerializer):
     user = StringRelatedField()
+    rating = IntegerField(read_only=True)
+    number_of_comments = IntegerField(read_only=True)
+    upvoted_users = StringRelatedField(read_only=True, many=True)
+    downvoted_users = StringRelatedField(read_only=True, many=True)
+    saved_by = StringRelatedField(read_only=True, many=True)
 
     class Meta:
         model = Post
@@ -12,20 +24,20 @@ class PostSerializer(ModelSerializer):
 
 
 class CommentSerializer(ModelSerializer):
+    id = IntegerField(read_only=True)
     post = CharField(required=True)
     author = SlugRelatedField(
         read_only=True,
         slug_field='name'
     )
-    parent_comment = CharField(required=False)
+    parent_comment = IntegerField(required=False)
+    rating = IntegerField(read_only=True)
+    upvoted_users = StringRelatedField(read_only=True, many=True)
+    downvoted_users = StringRelatedField(read_only=True, many=True)
+    saved_by = StringRelatedField(read_only=True, many=True)
+    replies = StringRelatedField(read_only=True, many=True)
 
     class Meta:
         model = Comment
         fields = '__all__'
         depth = 1
-
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = Guest
-        fields = ('name', 'email', 'phone', 'skype', 'is_banned', 'is_muted', 'last_ban_date', 'is_moderator')
--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,29 @@
 from django.test import TestCase
+from .views import *
 
-# Create your tests here.
-# TODO: Create damn tests
+
+class PostViewSetTest(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_retrieve(self):
+        pass
+
+    def test_list(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_partial_update(self):
+        pass
+
+    def test_destroy(self):
+        pass
