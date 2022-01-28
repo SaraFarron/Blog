c2442ecb66b62ff9abcb39415b39a006c501748c
Nikita Syulin <nikita_sulin@idpowers.com>
2022-01-28 23:04:04 +0700

    rate posts

--- api/views.py
+++ api/views.py
@@ -239,6 +239,22 @@ class RatePostView(PatchModelMixin, GenericViewSet):
     queryset = Post.objects.all()
     serializer_class = RatePostSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        partial = True
+        instance = self.get_object()
+        instance.rating += 1  # TODO wrong instance
+        instance.user.rating += 1
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 class RateCommentView(PatchModelMixin, GenericViewSet):
     """
