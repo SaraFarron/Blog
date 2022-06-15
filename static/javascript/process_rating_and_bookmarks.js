cf31e0eb7e9289f098d271ed8856bf17c522a05a
YaredFall <yaredfall@gmail.com>
2022-06-15 23:58:16 +0700

    notifications (login required), i18n, adjustments

--- static/javascript/process_rating_and_bookmarks.js
+++ static/javascript/process_rating_and_bookmarks.js
@@ -1,3 +1,5 @@
+const userIsAuthenticated = document.body.getAttribute('data-user-is-authenticated') === 'true';
+
 const ratingForms = document.querySelectorAll('[data-rating-form]');
 
 ratingForms.forEach((form) => {
@@ -7,47 +9,57 @@ ratingForms.forEach((form) => {
     let change = 0;
 
     form.querySelector('[data-rating-upvote]').addEventListener('click', () => {
-        if (!ratingLabel.classList.contains('animating')) {
-            ratingLabel.classList.add("animating");
-            actionField.setAttribute("value", "upvote");
-            const status = form.getAttribute("data-rating-form");
-            if (status === 'upvoted') {
-                change = -1;
-                form.setAttribute("data-rating-form", "unvoted");
-            }
-            else if (status === 'downvoted') {
-                change = +2;
-                form.setAttribute("data-rating-form", "upvoted");
-            }
-            else {
-                change = +1;
-                form.setAttribute("data-rating-form", "upvoted");
-            }
-            animLabel.innerHTML = change > 0 ? `+${change}` : change;
+        if (userIsAuthenticated) {
+            if (!ratingLabel.classList.contains('animating')) {
+                ratingLabel.classList.add("animating");
+                actionField.setAttribute("value", "upvote");
+                const status = form.getAttribute("data-rating-form");
+                if (status === 'upvoted') {
+                    change = -1;
+                    form.setAttribute("data-rating-form", "unvoted");
+                }
+                else if (status === 'downvoted') {
+                    change = +2;
+                    form.setAttribute("data-rating-form", "upvoted");
+                }
+                else {
+                    change = +1;
+                    form.setAttribute("data-rating-form", "upvoted");
+                }
+                animLabel.innerHTML = change > 0 ? `+${change}` : change;
 
-            form.submit();
+                form.submit();
+            }
+        }
+        else {
+            showNotification('login-required');
         }
     });
     form.querySelector('[data-rating-downvote]').addEventListener('click', () => {
-        if (!ratingLabel.classList.contains('animating')) {
-            ratingLabel.classList.add("animating");
-            actionField.setAttribute("value", "downvote");
-            const status = form.getAttribute("data-rating-form");
-            if (status === 'upvoted') {
-                change = -2;
-                form.setAttribute("data-rating-form", "downvoted");
-            }
-            else if (status === 'downvoted') {
-                change = +1;
-                form.setAttribute("data-rating-form", "unvoted");
-            }
-            else {
-                change = -1;
-                form.setAttribute("data-rating-form", "downvoted");
-            }
-            animLabel.innerHTML = change > 0 ? `+${change}` : change;
+        if (userIsAuthenticated) {
+            if (!ratingLabel.classList.contains('animating')) {
+                ratingLabel.classList.add("animating");
+                actionField.setAttribute("value", "downvote");
+                const status = form.getAttribute("data-rating-form");
+                if (status === 'upvoted') {
+                    change = -2;
+                    form.setAttribute("data-rating-form", "downvoted");
+                }
+                else if (status === 'downvoted') {
+                    change = +1;
+                    form.setAttribute("data-rating-form", "unvoted");
+                }
+                else {
+                    change = -1;
+                    form.setAttribute("data-rating-form", "downvoted");
+                }
+                animLabel.innerHTML = change > 0 ? `+${change}` : change;
 
-            form.submit();
+                form.submit();
+            }
+        }
+        else {
+            showNotification('login-required');
         }
     });
 
@@ -79,21 +91,26 @@ bookmarkForms.forEach((form) => {
     const icon_inactive = form.querySelector('[data-bookmark-icon-inactive]');
 
     form.addEventListener('click', () => {
-        if (!form.classList.contains('animating')) {
-            form.classList.add("animating");
-            const status = form.getAttribute("data-bookmark-form");
-            if (status === 'saved') {
-                icon_active.classList.add('hidden');
-                icon_inactive.classList.remove('hidden');
-                form.setAttribute("data-bookmark-form", "not-saved");
-            }
-            else if (status === 'not-saved') {
-                icon_active.classList.remove('hidden');
-                icon_inactive.classList.add('hidden');
-                form.setAttribute("data-bookmark-form", "saved");
-            }
+        if (userIsAuthenticated) {
+            if (!form.classList.contains('animating')) {
+                form.classList.add("animating");
+                const status = form.getAttribute("data-bookmark-form");
+                if (status === 'saved') {
+                    icon_active.classList.add('hidden');
+                    icon_inactive.classList.remove('hidden');
+                    form.setAttribute("data-bookmark-form", "not-saved");
+                }
+                else if (status === 'not-saved') {
+                    icon_active.classList.remove('hidden');
+                    icon_inactive.classList.add('hidden');
+                    form.setAttribute("data-bookmark-form", "saved");
+                }
 
-            form.submit();
+                form.submit();
+            }
+        }
+        else {
+            showNotification('login-required');
         }
     });
 
@@ -101,3 +118,14 @@ bookmarkForms.forEach((form) => {
         form.classList.remove('animating');
     });
 });
+
+function showNotification(templateName)
+{
+    notificationList = document.querySelector('[data-notification-list]');
+    notificationTemplate = document.querySelector(`[data-notification-template="${templateName}"]`);
+    if (notificationTemplate != null) {
+        let notificationElement = notificationTemplate.cloneNode(true);
+        notificationElement.style.display = "block";
+        notificationList.append(notificationElement);
+    }
+}
